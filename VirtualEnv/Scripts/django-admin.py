ffb7e933f8614ff73e260cc39ba1e46d128405ac
Juan <jgparot@uc.cl>
2020-04-05 23:51:56 -0400

    version heroku

---
+++ VirtualEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\juanp\desktop\universidad\2020-1\tarea1\virtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
